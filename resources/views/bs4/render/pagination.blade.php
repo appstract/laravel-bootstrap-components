d4c5d0caa846b26936bdff6fdd4def49bdf1e0d6
Olav van Schie <olavschie@hotmail.com>
2017-06-08 17:00:49 +0200

    Pagination render options

--- resources/views/bs4/render/pagination.blade.php
+++ resources/views/bs4/render/pagination.blade.php
@@ -3,13 +3,15 @@
         $paginator = $paginator->toArray();
     @endphp
 
-    <ul class="pagination">
-        <li class="page-item">
-            <a class="page-link" href="{{ $paginator['prev_page_url'] }}" aria-label="Previous">
-                <span aria-hidden="true">&laquo;</span>
-                <span class="sr-only">Previous</span>
-            </a>
-        </li>
+    <ul class="pagination {{ $class or '' }}">
+        @istrue($arrows)
+            <li class="page-item">
+                <a class="page-link" href="{{ $paginator['prev_page_url'] }}" aria-label="Previous">
+                    <span aria-hidden="true">&laquo;</span>
+                    <span class="sr-only">Previous</span>
+                </a>
+            </li>
+        @endistrue
 
         @foreach($elements[0] as $page => $link)
             <li class="page-item @if($paginator['current_page'] == $page) active @endif">
@@ -17,11 +19,13 @@
             </li>
         @endforeach
 
-        <li class="page-item">
-            <a class="page-link" href="{{ $paginator['next_page_url'] }}" aria-label="Next">
-                <span aria-hidden="true">&raquo;</span>
-                <span class="sr-only">Next</span>
-            </a>
-        </li>
+        @istrue($arrows)
+            <li class="page-item">
+                <a class="page-link" href="{{ $paginator['next_page_url'] }}" aria-label="Next">
+                    <span aria-hidden="true">&raquo;</span>
+                    <span class="sr-only">Next</span>
+                </a>
+            </li>
+        @endistrue
     </ul>
 </nav>
--- src/BootstrapComponentsClass.php
+++ src/BootstrapComponentsClass.php
@@ -16,18 +16,19 @@ class BootstrapComponentsClass
     /*
      * Render pagination buttons
      *
-     * @param     $items
-     * @param int $page
-     * @param int $perPage
-     * @param     $url
+     * @param       $items
+     * @param int   $page
+     * @param int   $perPage
+     * @param       $path
+     * @param array $componentOptions
      */
-    public function pagination($items, $page = 1, $perPage = 25, $path = '')
+    public function pagination($items, $page = 1, $perPage = 25, $path = '', array $componentOptions = [])
     {
         $items = $items instanceof Collection ? $items : Collection::make($items);
 
         $paginator = (new LengthAwarePaginator($items, $items->count(), (int) $perPage, (int) $page))
             ->withPath($path);
 
-        return $paginator->render('bootstrap-components::render.pagination');
+        return $paginator->render('bootstrap-components::render.pagination', $componentOptions);
     }
 }
